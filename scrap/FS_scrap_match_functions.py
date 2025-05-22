ec3bac5f65720083c051aa78072cdf5afb685c3a
Paula Saavedra <50970099+paulasaavedra@users.noreply.github.com>
2025-05-22 14:29:54 -0300

    Update FS_scrap_match_functions.py

--- scrap/FS_scrap_match_functions.py
+++ scrap/FS_scrap_match_functions.py
@@ -1204,7 +1204,7 @@ for filename in [csv_files]:
             more_matches = False
 
         list_of_matches = tourney_to_matchs(link_df,more_matches)
-        list_of_matches = list_of_matches[:-108]
+        list_of_matches = list_of_matches[:-130]
         
         
 
