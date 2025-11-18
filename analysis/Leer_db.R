4e9e92e0002755ac14cecf8117d8319516ff4619
Paula Saavedra <50970099+paulasaavedra@users.noreply.github.com>
2025-11-17 22:21:00 -0300

    Actualizar procesamiento de datos de partidos y ajustar rutas de archivos CSV para 2025

--- analysis/Leer_db.R
+++ analysis/Leer_db.R
@@ -22,8 +22,8 @@ setnames(db <- setDT(dbReadTable(con, "atp_simple")), tolower(names(db)))
 # setnames(pbyp <- setDT(dbReadTable(con, "atp_pbyp")), tolower(names(pbyp)))
 setnames(rank <- setDT(dbReadTable(con, "atp_rankings")), tolower(names(rank)))
 
-setnames(db_stats_w_t <- setDT(dbReadTable(con, "atp_stats_w_t")), tolower(names(db_stats_w_t)))
-setnames(db_stats_l_t <- setDT(dbReadTable(con, "atp_stats_l_t")), tolower(names(db_stats_l_t)))
+#setnames(db_stats_w_t <- setDT(dbReadTable(con, "atp_stats_w_t")), tolower(names(db_stats_w_t)))
+#setnames(db_stats_l_t <- setDT(dbReadTable(con, "atp_stats_l_t")), tolower(names(db_stats_l_t)))
 
 
 # Cerrar la conexión
